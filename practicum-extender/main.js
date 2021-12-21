dde69b6fc14490f28b0e38ba5b655956e8be3e92
Sergey Arefiev <info@areh.ru>
2021-12-21 12:24:26 +0500

    Горячие клавиши: используем выбор по селектору каждый раз, вместо сохранения ссылки, т.к. после перехода на новую тему, старых элементов на странице не остается

--- practicum-extender/main.js
+++ practicum-extender/main.js
@@ -1,13 +1,15 @@
 // Итоговый вариант, умеющий ставить фокус на редактор кода + нажимать последнюю кнопку в панели (проверка или переход к следующему заданию \ занятию)
-document.addEventListener('keydown', function(event) {
-  if (event.code == 'Backslash' && (event.ctrlKey || event.metaKey)){
-	if (!document.shortKeys) document.shortKeys = {};
-	if (!document.shortKeys.editor) document.shortKeys.editor = document.querySelector('.code-editor textarea');
-	if (!document.shortKeys.btnPanel) document.shortKeys.btnPanel = document.querySelector('.trainer-footer__actions');	
-	if (event.shiftKey)
-		document.shortKeys.btnPanel.children[document.shortKeys.btnPanel.childElementCount - 1].click();
-	else
-		document.shortKeys.editor.focus();
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'Backslash' && (event.ctrlKey || event.metaKey)) {
+    if (event.shiftKey) {
+      let btnPanel = document.querySelector('.trainer-footer__actions');
+      if (!btnPanel) return;
+      btnPanel.children[btnPanel.childElementCount - 1].click();
+    } else {
+      let editor = document.querySelector('.code-editor textarea');
+      if (!editor) return;
+      editor.focus();
+    }
   }
 });
 
